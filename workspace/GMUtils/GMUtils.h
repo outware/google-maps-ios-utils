8ce94efa3ef9fa1fad98823340fa4e1991072772
Tristan Burnside <tristan.burnside@outware.com.au>
2016-12-21 14:59:31 +1100

    Create a static framework version of the library

---
+++ workspace/GMUtils/GMUtils.h
@@ -0,0 +1,36 @@
+//
+//  GMUtils.h
+//  GMUtils
+//
+//  Created by Tristan Burnside on 13/12/2016.
+//  Copyright © 2016 Google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GMUtils.
+FOUNDATION_EXPORT double GMUtilsVersionNumber;
+
+//! Project version string for GMUtils.
+FOUNDATION_EXPORT const unsigned char GMUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GMUtils/PublicHeader.h>
+
+#import <GMUtils/GMUStaticCluster.h>
+#import <GMUtils/GMUClusterAlgorithm.h>
+#import <GMUtils/GMUGridBasedClusterAlgorithm.h>
+#import <GMUtils/GMUNonHierarchicalDistanceBasedAlgorithm.h>
+#import <GMUtils/GMUSimpleClusterAlgorithm.h>
+#import <GMUtils/GMUWrappingDictionaryKey.h>
+#import <GMUtils/GMUClusterItem.h>
+#import <GMUtils/GMUClusterManager.h>
+#import <GMUtils/GMUStaticCluster.h>
+#import <GMUtils/GMUClusterIconGenerator.h>
+#import <GMUtils/GMUClusterRenderer.h>
+#import <GMUtils/GMUDefaultClusterIconGenerator.h>
+#import <GMUtils/GMUDefaultClusterRenderer.h>
+#import <GMUtils/GQTBounds.h>
+#import <GMUtils/GQTPoint.h>
+#import <GMUtils/GQTPointQuadTree.h>
+#import <GMUtils/GQTPointQuadTreeChild.h>
+#import <GMUtils/GQTPointQuadTreeItem.h>
